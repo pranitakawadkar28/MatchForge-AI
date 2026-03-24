16a1b7eff65115c0b7d5c6097395275ba437b101
Pranita Kawadkar <pranitakawadkar10@gmail.com>
2026-03-24 22:13:36 +0530

    refactor(server-app): integrate cookie parser and global error handling middleware

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import authRouter from "./routes/auth/auth.routes.js"
+
+import { errorHandler } from "./middleware/error.middleware.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+
+app.use(errorHandler);
 
 export default app;
