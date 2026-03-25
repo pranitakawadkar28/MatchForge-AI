a5fda3fe317823689b1716dbbdd00f021796ff13
Pranita Kawadkar <pranitakawadkar10@gmail.com>
2026-03-25 16:08:00 +0530

    feat(auth): implement CORS configuration with credentials support

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRouter from "./routes/auth/auth.routes.js"
-
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
+const corsOptions = {
+  origin: ["http://localhost:3000"], 
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
